0360f9600f99e11fb476cda468417f5b93866813
FIRST_NAME LAST_NAME <6069darshan@gmail.com>
2024-03-11 22:44:16 -0800

    first commit

---
+++ lib/mdelclass.dart
@@ -0,0 +1,14 @@
+
+
+
+class BookmarkModel {
+   var bookmark;
+   var bookmarktitle;
+
+   BookmarkModel({
+      required this.bookmarktitle,
+      required this.bookmark,
+   });
+
+}
+
---
+++ lib/mirrorwallprovider.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+
+import 'mdelclass.dart';
+
+class mirrorWallProvider with ChangeNotifier {
+  ConnectivityResult connectionStatus = ConnectivityResult.none;
+  final Connectivity connectivity = Connectivity();
+  late StreamSubscription<ConnectivityResult> connectivitySubscription;
+
+
+  List<BookmarkModel> bookMarkList = [];
+
+  Future<void> initConnectivity() async {
+    late ConnectivityResult result;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      result = await connectivity.checkConnectivity();
+    } on PlatformException catch (e) {
+     // developer.log('Couldn\'t check connectivity status', error: e);
+      return;
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    return updateConnectionStatus(result);
+  }
+
+  Future<void> updateConnectionStatus(ConnectivityResult result) async {
+    connectionStatus = result;
+    notifyListeners();
+  }
+
+  bookMarkADD(BookmarkModel DATA){
+    bookMarkList.add(DATA);
+    notifyListeners();
+  }
+
+  deleteBookMark(index){
+    bookMarkList.removeAt(index);
+    notifyListeners();
+  }
+
+}

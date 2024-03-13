710e525471550916eaa43258c18b25977152c1fb
Darshan6069 <138776334+Darshan6069@users.noreply.github.com>
2024-03-13 02:08:57 -0800

    Update mirrorwallprovider.dart

--- lib/mirrorwallprovider.dart
+++ lib/mirrorwallprovider.dart
@@ -3,13 +3,18 @@ import 'dart:async';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
+import 'main.dart';
 import 'mdelclass.dart';
 
 class mirrorWallProvider with ChangeNotifier {
   ConnectivityResult connectionStatus = ConnectivityResult.none;
   final Connectivity connectivity = Connectivity();
   late StreamSubscription<ConnectivityResult> connectivitySubscription;
+
+  SearchEngine? Engine = SearchEngine.Google;
+  InAppWebViewController? webViewController;
 
 
   List<BookmarkModel> bookMarkList = [];
@@ -42,6 +47,19 @@ class mirrorWallProvider with ChangeNotifier {
 
   deleteBookMark(index){
     bookMarkList.removeAt(index);
+    notifyListeners();
+  }
+
+
+
+  bookMarkUrl(index){
+    webViewController!.loadUrl(urlRequest: URLRequest(url: WebUri('${bookMarkList[index].bookmark}')));
+notifyListeners();
+  }
+
+
+  changeEngine(value){
+    Engine = value;
     notifyListeners();
   }
 
